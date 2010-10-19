e6be71cc743a0715f933d6a294366d070297a2a5
Ugo Cupcic <ugo@shadowrobot.com>
2010-10-19 15:27:58 +0100

    grasp chooser. Need to implement the save grasp function.

--- shadow_robot/sr_control_gui/src/sr_control_gui.2.0/plugins/grasp_controller/__init__.py
+++ shadow_robot/sr_control_gui/src/sr_control_gui.2.0/plugins/grasp_controller/__init__.py
@@ -8,44 +8,103 @@ from Grasp import Grasp
 from grasps_interpoler import GraspInterpoler
 from grasps_parser import GraspParser
 
-class GraspChooser(QtGui.QListWidget):
-    def __init__(self, parent=None):
-        QtGui.QListWidget.__init__(self)
-
-        self.parent = parent
-        
-        self.setViewMode(QtGui.QListView.ListMode)
-        self.setResizeMode(QtGui.QListView.Adjust)
-
-        #sublayout = QtGui.QVBoxLayout()
-        #self.frame = QtGui.QFrame()
-        #self.list = QtGui.QListWidget()
-        #tree.setColumnCount(1)
+class GraspChooser(QtGui.QWidget):
+    def __init__(self, parent, plugin_parent, title):
+        QtGui.QWidget.__init__(self)
+        self.plugin_parent = plugin_parent
+        self.grasp = None
+        self.title = QtGui.QLabel()
+        self.title.setText(title)
     
     def draw(self):
-        for grasp_name in self.parent.sr_library.grasp_parser.grasps.keys():
-            self.addItem( QtGui.QListWidgetItem(grasp_name))    
-        self.connect(self, QtCore.SIGNAL('itemClicked(QListWidgetItem*)'), self.grasp_choosed)
+        self.frame = QtGui.QFrame(self)
+
+        self.list = QtGui.QListWidget()
+        first_item = None
+        for grasp_name in self.plugin_parent.sr_library.grasp_parser.grasps.keys():
+            item = QtGui.QListWidgetItem(grasp_name)
+            if first_item == None:
+                first_item = item
+            self.list.addItem(item)    
+        self.connect(self.list, QtCore.SIGNAL('itemClicked(QListWidgetItem*)'), self.grasp_choosed)
+        self.list.setViewMode(QtGui.QListView.ListMode)
+        self.list.setResizeMode(QtGui.QListView.Adjust)
+        self.list.setItemSelected(first_item, True)
+        self.grasp_choosed(first_item, first_time = True)
         
-        #sublayout.addWidget(self.list)
+        self.layout = QtGui.QVBoxLayout()
+        self.layout.addWidget(self.title)
+        self.layout.addWidget(self.list)
         
-        #self.frame.setLayout(sublayout)
-        #self.show()
+        self.frame.setLayout(self.layout)
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.frame)
+        self.frame.show()
+        self.setLayout(layout)
+        self.show()
         
-    def grasp_choosed(self, item):
-        print item.text()
+    def grasp_choosed(self, item, first_time = False):
+        self.grasp = self.plugin_parent.sr_library.grasp_parser.grasps[str(item.text())]
+        if not first_time:
+            self.plugin_parent.grasp_changed()
         #print grasp_name
         
 class GraspSlider(QtGui.QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent, plugin_parent):
         QtGui.QWidget.__init__(self, parent)
+        self.plugin_parent = plugin_parent
         
+    def draw(self):
+        self.frame = QtGui.QFrame(self)
+        label_frame = QtGui.QFrame(self.frame)
+        from_label = QtGui.QLabel()
+        from_label.setText("From")
+        ref_label = QtGui.QLabel()
+        ref_label.setText("Reference")
+        to_label = QtGui.QLabel()
+        to_label.setText("To")
+        label_layout = QtGui.QHBoxLayout()
+        label_layout.addWidget(from_label)
+        label_layout.addWidget(ref_label)
+        label_layout.addWidget(to_label)
+        
+        label_frame.setLayout(label_layout)
+        
+        self.slider = QtGui.QSlider()
+        self.slider.setOrientation(QtCore.Qt.Horizontal)
+        self.slider.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.slider.setTickInterval(100)
+        self.slider.setTickPosition(Qt.QSlider.TicksAbove)
+        self.slider.setMinimum(-100)
+        self.slider.setMaximum(100)
+        
+        self.connect(self.slider, QtCore.SIGNAL('valueChanged(int)'), self.changeValue)
+        
+        self.layout = QtGui.QVBoxLayout()
+        self.layout.addWidget(label_frame)
+        self.layout.addWidget(self.slider)
+                
+        self.frame.setLayout(self.layout)
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.frame)
+        self.frame.show()
+        self.setLayout(layout)
+        self.show()
+        
+    def changeValue(self, value):
+        self.plugin_parent.interpolate_grasps(value)
 
 class GraspController(ShadowGenericPlugin):  
     name = "Grasp Controller"
         
     def __init__(self):        
         ShadowGenericPlugin.__init__(self)
+        
+        self.current_grasp = Grasp()
+        self.current_grasp.name = 'CURRENT_UNSAVED'
+        self.grasp_interpoler_1 = None
+        self.grasp_interpoler_2 = None
+        
         self.set_icon('images/icons/iconHand.png')
         
         self.frame = QtGui.QFrame()
@@ -54,7 +113,7 @@ class GraspController(ShadowGenericPlugin):
         subframe = QtGui.QFrame()
         sublayout = QtGui.QVBoxLayout()
          
-        self.grasp_slider = GraspSlider()
+        self.grasp_slider = GraspSlider(self.frame, self)
         sublayout.addWidget(self.grasp_slider)
         
         btn_frame = QtGui.QFrame()
@@ -74,22 +133,45 @@ class GraspController(ShadowGenericPlugin):
         sublayout.addWidget(btn_frame)
         subframe.setLayout(sublayout)
          
+        self.grasp_from_chooser = GraspChooser(self.window, self, "From: ")
+        self.layout.addWidget(self.grasp_from_chooser)
         self.layout.addWidget(subframe)
         
-        self.grasp_chooser = GraspChooser(self)
-        self.layout.addWidget(self.grasp_chooser)
+        self.grasp_to_chooser = GraspChooser(self.window, self, "To: ")
+        self.layout.addWidget(self.grasp_to_chooser)
              
     def activate(self):
         ShadowGenericPlugin.activate(self)
-        self.frame.show()  
+
         self.frame.setLayout(self.layout)
         self.window.setWidget(self.frame)
-        self.grasp_chooser.draw()
+        self.grasp_slider.draw()
+        self.grasp_to_chooser.draw()
+        self.grasp_from_chooser.draw()
+        self.set_reference_grasp()
         Qt.QTimer.singleShot(0, self.window.adjustSize)
-        
         
     def save_grasp(self):
         print "save grasp"
     
     def set_reference_grasp(self):
-        print "set ref grasp"
+        self.current_grasp.joints_and_positions = self.sr_library.read_all_current_positions()
+        
+        self.grasp_interpoler_1 = GraspInterpoler(self.grasp_from_chooser.grasp, self.current_grasp)
+        self.grasp_interpoler_2 = GraspInterpoler(self.current_grasp, self.grasp_to_chooser.grasp)
+        
+        self.grasp_slider.slider.setValue(0)
+    
+    def grasp_changed(self):
+        self.current_grasp.joints_and_positions = self.sr_library.read_all_current_positions()
+        self.grasp_interpoler_1 = GraspInterpoler(self.grasp_from_chooser.grasp, self.current_grasp)
+        self.grasp_interpoler_2 = GraspInterpoler(self.current_grasp, self.grasp_to_chooser.grasp)    
+    
+    def interpolate_grasps(self, value):
+        #from -> current
+        if value < 0:
+            targets_to_send = self.grasp_interpoler_1.interpolate(100 + value)
+            self.sr_library.sendupdate_from_dict(targets_to_send)
+        else:   #current -> to
+            targets_to_send = self.grasp_interpoler_2.interpolate(value)
+            self.sr_library.sendupdate_from_dict(targets_to_send)
