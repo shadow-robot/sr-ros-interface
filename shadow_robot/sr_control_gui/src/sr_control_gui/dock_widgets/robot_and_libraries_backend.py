be884b8247a5d5462fcdb9c0e86f97283090d1ed
Ugo Cupcic <ugo@shadowrobot.com>
2010-10-29 17:08:17 +0100

    added ssh client + ask for login / pwd when trying to log to a remote computer + checking login / pwd

--- shadow_robot/sr_control_gui/src/sr_control_gui/dock_widgets/robot_and_libraries_backend.py
+++ shadow_robot/sr_control_gui/src/sr_control_gui/dock_widgets/robot_and_libraries_backend.py
@@ -4,6 +4,7 @@ import roslib; roslib.load_manifest('sr_control_gui')
 from PyQt4 import QtCore, QtGui, Qt
 import rospy
 
+import paramiko
 import subprocess
 import socket
 import time
@@ -19,39 +20,60 @@ class RunCommand(Qt.QThread):
     def run(self):
         #TODO run the command
         if self.command != "":
-            process = subprocess.Popen(self.command.split(), stdout=subprocess.PIPE)
-            output = process.communicate()[0]
-            if self.final_status == "get_status":
-                output = output.split('\n')
-                
-                all_name_found = True
-                for name in self.library.list_of_nodes:
-                    name_found = False
-                    for line in output:
-                        if name in output:
-                            name_found = True
-                            #remove the line from the output
-                            output.pop(output.index(name))
-                            break
-                    if not name_found:
-                        all_name_found = False
-                        break
-                if all_name_found:
-                    self.library.status = "started"
-                else:
-                    if "starting" != self.library.status:
-                        self.library.status = "stopped"
-                
+            if self.library.is_local:
+                self.run_local_command()
+            elif self.final_status == "get_status":
+                #the get status command is a ros command run locally
+                self.run_local_command()
             else:
-                self.library.status = self.final_status
+                self.run_ssh_command()
+    
+    def run_local_command(self):
+        process = subprocess.Popen(self.command.split(), stdout=subprocess.PIPE)
+        output = process.communicate()[0]
+        if self.final_status == "get_status":
+            output = output.split('\n')
+            
+            all_name_found = True
+            for name in self.library.list_of_nodes:
+                name_found = False
+                for line in output:
+                    if name in output:
+                        name_found = True
+                        #remove the line from the output
+                        output.pop(output.index(name))
+                        break
+                if not name_found:
+                    all_name_found = False
+                    break
+            if all_name_found:
+                self.library.status = "started"
+            else:
+                if "starting" != self.library.status:
+                    self.library.status = "stopped"
+            
+        else:
+            self.library.status = self.final_status
+
+    def run_ssh_command(self):
+        self.ssh_client.connect(self.library.ip,
+                                username=self.library.login,
+                                password=self.library.password)
         
 class Library(object):
-    def __init__(self, name="", list_of_nodes=[], start_cmd="", stop_cmd="", status_cmd=""):
+    def __init__(self, name="", list_of_nodes=[], start_cmd="", stop_cmd="", status_cmd="", root_path=""):
         self.name = name
         self.ip = ""
         self.hostname = ""
         self.status = ""
+        self.login = ""
+        self.password = ""
+        self.icon_local_path = root_path + '/src/sr_control_gui/images/icons/local.png'
+        self.icon_remote_path = root_path + '/src/sr_control_gui/images/icons/remote.png'
+        self.icon_library_path = root_path + '/src/sr_control_gui/images/icons/ros.jpg'
         self.is_local = True
+        self.ssh_client = paramiko.SSHClient()
+        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.list_of_nodes = list_of_nodes
         self.start_cmd = start_cmd
         self.stop_cmd = stop_cmd
@@ -66,16 +88,18 @@ class Library(object):
         self.set_ip(ip)
         self.is_local = True
     
-    def set_ip(self, ip):
+    def set_ip(self, ip, login="", password=""):
         try:
             socket.inet_aton(ip)
         except socket.error:
             # not a valid ip
-            return -1
+            return - 1
         self.ip = ip
         try:
             self.hostname = socket.gethostbyaddr(str(ip))[0]
             if self.hostname != socket.gethostbyname(socket.gethostname()):
+                self.login = login
+                self.password = password
                 self.is_local = False
             else:
                 self.is_local = True
@@ -93,14 +117,24 @@ class Library(object):
     def stop(self):
         self.status = "stopping"
         self.thread_stop.start()
+    
+    
+    def test_login_pwd(self, login, pwd):
+        try:
+            self.ssh_client.connect(self.ip, username=login, password=pwd)
+        except:
+            print "Error: ", sys.exc_info()[0]
+            return False
+        self.ssh_client.close()
+        return True
 
 class RobotAndLibrariesBackend(object):
     def __init__(self):
         self.libraries = {}
         
-    def add_library(self, name, list_of_nodes=[], start_cmd="", stop_cmd="", status_cmd=""):
+    def add_library(self, name, list_of_nodes=[], start_cmd="", stop_cmd="", status_cmd="", root_path=""):
         lib = Library(name=name, list_of_nodes=list_of_nodes, start_cmd=start_cmd,
-                      stop_cmd=stop_cmd, status_cmd=status_cmd)
+                      stop_cmd=stop_cmd, status_cmd=status_cmd, root_path=root_path)
         self.libraries[name] = lib
         
     
--- shadow_robot/sr_control_gui/src/sr_control_gui/dock_widgets/robot_and_libraries_dock_widget.py
+++ shadow_robot/sr_control_gui/src/sr_control_gui/dock_widgets/robot_and_libraries_dock_widget.py
@@ -11,9 +11,99 @@ from robot_and_libraries_backend import RobotAndLibrariesBackend, Library
 library_refresh_rate = 0.5
 
 
+class LoginForm(QtGui.QDialog):
+    def __init__(self, parent, treeitem, title, library):
+        QtGui.QDialog.__init__(self, parent)
+        self.library = library
+        self.treeitem = treeitem
+        
+        self.setWindowTitle(title)
+        self.setModal(True)
+
+        dialog_frame = QtGui.QFrame(self)
+        dialog_layout = QtGui.QGridLayout()
+        
+        label_login = QtGui.QLabel(dialog_frame)
+        label_login.setText("Login: ")
+        self.login_input = QtGui.QLineEdit(dialog_frame)    
+        dialog_layout.addWidget(label_login, 0, 0)
+        dialog_layout.addWidget(self.login_input, 0, 1)    
+        
+        dialog_frame.connect(self.login_input, QtCore.SIGNAL('textChanged(QString)'),
+                             self.login_changed)
+        
+        label_pwd = QtGui.QLabel(dialog_frame)
+        label_pwd.setText("Password: ")
+        self.pwd_input = QtGui.QLineEdit(dialog_frame)
+        self.pwd_input.setEchoMode(QtGui.QLineEdit.Password)
+        dialog_layout.addWidget(label_pwd, 1, 0)
+        dialog_layout.addWidget(self.pwd_input, 1, 1)
+ 
+        dialog_frame.connect(self.pwd_input, QtCore.SIGNAL('textChanged(QString)'),
+                             self.pwd_changed)
+        
+        dialog_frame.setLayout(dialog_layout)
+        
+        btn_frame = QtGui.QFrame()   
+        self.btn_ok = QtGui.QPushButton(btn_frame)
+        self.btn_ok.setText("OK")
+        self.btn_ok.setDisabled(True)
+        self.connect(self.btn_ok, QtCore.SIGNAL("clicked()"), self.accept)
+        btn_cancel = QtGui.QPushButton(btn_frame)
+        btn_cancel.setText("Cancel")
+        self.connect(btn_cancel, QtCore.SIGNAL("clicked()"), self.reject)
+        
+        btn_layout = QtGui.QHBoxLayout()
+        btn_layout.addWidget(self.btn_ok)
+        btn_layout.addWidget(btn_cancel)
+        btn_frame.setLayout(btn_layout)
+        
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(dialog_frame)
+        layout.addWidget(btn_frame)
+        self.setLayout(layout)
+
+        self.show()
+    
+    def login_changed(self, login):
+        if login != "":
+            if str(self.pwd_input.text()) != "":
+                self.btn_ok.setEnabled(True)
+                return
+        self.btn_ok.setDisabled(True)
+        
+    def pwd_changed(self, pwd):
+        if pwd != "":
+            if str(self.login_input.text()) != "":
+                self.btn_ok.setEnabled(True)
+                return
+        self.btn_ok.setDisabled(True)
+    
+    def accept(self):
+        login = str(self.login_input.text())
+        pwd = str(self.pwd_input.text())
+        
+        good_pwd = self.library.test_login_pwd(login, pwd)
+        
+        if good_pwd:
+            self.library.login = login
+            self.library.password = pwd
+            self.treeitem.reboot_computer_action.setEnabled(True)
+            self.treeitem.setIcon(0, QtGui.QIcon(self.library.icon_remote_path))
+        else:
+            rospy.logerr("Wrong password - setting local IP")
+            self.library.set_local_ip()
+            self.treeitem.setData(3, QtCore.Qt.DisplayRole, self.library.hostname)
+            self.treeitem.reboot_computer_action.setDisabled(True)
+            self.treeitem.setIcon(0, QtGui.QIcon(self.library.icon_local_path))
+            QtGui.QDialog.reject(self)
+        
+        QtGui.QDialog.accept(self)
+
 class LibraryItem(QtGui.QTreeWidgetItem):
-    def __init__(self, library, parent, icon_path=""):
+    def __init__(self, library, parent):
         data = []
+        data.append("")
         data.append(library.name)
         data.append(library.status)
         data.append(library.hostname)
@@ -21,8 +111,7 @@ class LibraryItem(QtGui.QTreeWidgetItem):
         self.parent = parent
         QtGui.QTreeWidgetItem.__init__(self, data)
         
-        if icon_path != "":
-            self.setIcon(0, QtGui.QIcon(icon_path))
+        self.setIcon(1, QtGui.QIcon(self.library.icon_library_path))
         self.menu = QtGui.QMenu(data[0])
         
         self.green = QtGui.QColor(153, 231, 96)
@@ -44,6 +133,7 @@ class LibraryItem(QtGui.QTreeWidgetItem):
         
         if self.library.is_local:
             self.reboot_computer_action.setDisabled(True)
+            self.setIcon(0, QtGui.QIcon(self.library.icon_local_path))
         
         self.menu.addAction(self.start_lib)
         self.menu.addAction(self.stop_lib)
@@ -58,27 +148,27 @@ class LibraryItem(QtGui.QTreeWidgetItem):
             
     def edit_ip(self):
         self.setFlags(self.flags() | QtCore.Qt.ItemIsEditable)
-        self.parent.editItem(self, 2)
+        self.parent.editItem(self, 3)
     
     def refresh_status(self):
         self.library.get_status()
         current_status = self.library.status
-        self.setData(1, QtCore.Qt.DisplayRole, current_status)
+        self.setData(2, QtCore.Qt.DisplayRole, current_status)
         if "ing" in current_status:
             self.start_lib.setDisabled(True)
             self.stop_lib.setDisabled(True)
             
-            self.setBackgroundColor(0, QtGui.QColor(self.orange))
+            self.setBackgroundColor(2, QtGui.QColor(self.orange))
         if current_status == "started":
             self.start_lib.setDisabled(True)
             self.stop_lib.setEnabled(True)
             
-            self.setBackgroundColor(0, QtGui.QColor(self.green))
+            self.setBackgroundColor(2, QtGui.QColor(self.green))
         if current_status == "stopped":
             self.start_lib.setEnabled(True)
             self.stop_lib.setDisabled(True)
             
-            self.setBackgroundColor(0, QtGui.QColor(self.red))
+            self.setBackgroundColor(2, QtGui.QColor(self.red))
             
     def setData(self, column, role, value):
         """
@@ -86,7 +176,7 @@ class LibraryItem(QtGui.QTreeWidgetItem):
         """
         QtGui.QTreeWidgetItem.setData(self, column, role, value)
         # if the ip address is changed
-        if column == 2:
+        if column == 3:
             try:
                 value = str(value.toString())
                 self.change_ip(value)
@@ -98,12 +188,12 @@ class LibraryItem(QtGui.QTreeWidgetItem):
 
     def stop_library(self):
         self.library.stop()
-        self.setData(1, QtCore.Qt.DisplayRole, "stopping")
+        self.setData(2, QtCore.Qt.DisplayRole, "stopping")
         self.setBackgroundColor(0, QtGui.QColor(self.orange))
 
     def start_library(self):
         self.library.start()
-        self.setData(1, QtCore.Qt.DisplayRole, "starting")
+        self.setData(2, QtCore.Qt.DisplayRole, "starting")
         self.setBackgroundColor(0, QtGui.QColor(self.orange))
 
     def reboot_computer(self):
@@ -113,12 +203,13 @@ class LibraryItem(QtGui.QTreeWidgetItem):
         if self.library.set_ip(new_ip) == -1:
             rospy.logerr("Bad IP address specified - setting local IP")
             self.library.set_local_ip()
-        self.setData(2, QtCore.Qt.DisplayRole, self.library.hostname)
+        self.setData(3, QtCore.Qt.DisplayRole, self.library.hostname)
         if self.library.is_local:
             self.reboot_computer_action.setDisabled(True)
+            self.setIcon(0, QtGui.QIcon(self.library.icon_local_path))
         else:
-            self.reboot_computer_action.setEnabled(True)
-
+            LoginForm(self.parent, self, self.library.hostname + " login", self.library)
+           
 class LibrariesWidget(QtGui.QWidget):
     def __init__(self, parent):
         QtGui.QWidget.__init__(self, parent=parent)
@@ -136,7 +227,8 @@ class LibrariesWidget(QtGui.QWidget):
                                                      list_of_nodes=list_of_nodes,
                                                      start_cmd="roslaunch sr_hand srh_motor.launch",
                                                      stop_cmd="rosnode kill " + " ".join(list_of_nodes),
-                                                     status_cmd="rosnode list")
+                                                     status_cmd="rosnode list",
+                                                     root_path=self.parent().parent().rootPath)
         
         list_of_nodes = ["/shadowarm",
                           "/shadowhand",
@@ -153,47 +245,57 @@ class LibrariesWidget(QtGui.QWidget):
                                                      list_of_nodes=list_of_nodes,
                                                      start_cmd="roslaunch sr_hand sr_arm_motor.launch",
                                                      stop_cmd="rosnode kill " + " ".join(list_of_nodes),
-                                                     status_cmd="rosnode list")
+                                                     status_cmd="rosnode list",
+                                                     root_path=self.parent().parent().rootPath)
         
         list_of_nodes = ["/cyberglove"]
         self.robot_and_libraries_backend.add_library("Cyberglove",
                                                      list_of_nodes=list_of_nodes,
                                                      start_cmd="roslaunch cyberglove cyberglove.launch",
                                                      stop_cmd="rosnode kill " + " ".join(list_of_nodes),
-                                                     status_cmd="rosnode list")
+                                                     status_cmd="rosnode list",
+                                                     root_path=self.parent().parent().rootPath)
         
         list_of_nodes = ["/cyberglove_remapper"]
         self.robot_and_libraries_backend.add_library("Glove Remapper",
                                                      list_of_nodes=list_of_nodes,
                                                      start_cmd="roslaunch sr_remappers remapper_glove.launch",
                                                      stop_cmd="rosnode kill " + " ".join(list_of_nodes),
-                                                     status_cmd="rosnode list")
+                                                     status_cmd="rosnode list",
+                                                     root_path=self.parent().parent().rootPath)
 
         self.tree = QtGui.QTreeWidget()
         self.tree.setEditTriggers(Qt.QAbstractItemView.DoubleClicked)
         
-        self.tree.setHeaderLabels(["ROS Nodes / Robot Code", "Status", "Computer"])
+        self.tree.setHeaderLabels(["", "ROS Nodes / Robot Code", "Status", "Computer"])
         items = []
         
-        icon_path = self.parent().parent().rootPath + '/src/sr_control_gui/images/icons/ros.jpg'
         for lib in self.robot_and_libraries_backend.libraries.values():
-            items.append(LibraryItem(lib, self.tree, icon_path=icon_path))
+            items.append(LibraryItem(lib, self.tree))
         
         self.tree.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         self.connect(self.tree,
                      QtCore.SIGNAL("customContextMenuRequested(const QPoint &)"),
                      self.showContextMenu) 
                 
-        #self.connect(self.tree, QtCore.SIGNAL('itemDoubleClicked (QTreeWidgetItem *, int)'), 
-        #             self.tree.editItem)
+        self.connect(self.tree, QtCore.SIGNAL('itemDoubleClicked (QTreeWidgetItem *, int)'),
+                     self.edit_ip)
         for item in items:
             self.tree.addTopLevelItem(item)
         listframe_layout.addWidget(self.tree)
+        
+        self.tree.resizeColumnToContents(0)
+        self.tree.resizeColumnToContents(1)
+        #don't resize col 2 and 3 as they contain dynamic content
+        
         list_frame.setLayout(listframe_layout)
 
         layout.addWidget(list_frame)
         self.setLayout(layout)
-                
+    
+    def edit_ip(self, item, value):
+        item.edit_ip()
+    
     def showContextMenu(self, point):
         item = self.tree.itemAt(point)
         if item == None:
