c1b4b0d41ce0962368681ab06cca762f57877587
Ugo Cupcic <ugo@shadowrobot.com>
2011-01-19 10:41:45 +0000

    updated the grasp / pregrasp positions for a top approach of the coke can (the joint targets are still stupid)

--- database/Copy_wine_grasp_to_coke.sql
+++ database/Copy_wine_grasp_to_coke.sql
@@ -10,4 +10,8 @@ grasp_grasp_joints='{0,0,0,50,20,20,0,50,20,20,0,50,20,20,0,0,50,20,20,30,0,0,30
 grasp_cluster_rep=true
 where grasp_id=1170000;
 
+UPDATE grasp
+set grasp_pregrasp_position='{0,0,0.32,0.7071,0,0.7071,0}',
+grasp_grasp_position='{0,0,0.3,0.7071,0,0.7071,0}'
+where grasp_id=1170000
 
