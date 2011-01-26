3dfed52861d31ea37af870146c7a142139de0a4c
Ugo Cupcic <ugo@shadowrobot.com>
2011-01-26 11:47:52 +0000

    started to code the simple action server.

---
+++ shadow_robot/sr_move_arm/include/sr_move_arm/move_arm_simple_action.hpp
@@ -0,0 +1,46 @@
+/**
+ * @file   move_arm_simple_action.hpp
+ * @author Ugo Cupcic <ugo@shadowrobot.com>, Contact <contact@shadowrobot.com>
+ * @date   Wed Jan 26 10:32:10 2011
+ *
+ * @brief  
+ *
+ */
+
+#ifndef SR_MOVE_ARM_SIMPLE_ACTION_H
+#define SR_MOVE_ARM_SIMPLE_ACTION_H
+
+#include <ros/ros.h>
+#include <boost/smart_ptr.hpp>
+#include <sr_hand/sendupdate.h>
+#include <actionlib/server/simple_action_server.h>
+#include <move_arm_msgs/MoveArmAction.h>
+
+using namespace ros;
+using namespace shadowrobot;
+
+namespace shadowrobot
+{
+  class SrMoveArmSimpleAction
+  {
+  public:
+    SrMoveArmSimpleAction();
+    ~SrMoveArmSimpleAction();
+
+  protected:
+    NodeHandle nh;
+    Publisher pub;
+    void execute(const move_arm_msgs::MoveArmGoalConstPtr& Goal);
+    
+    boost::shared_ptr<actionlib::SimpleActionServer<move_arm_msgs::MoveArmAction> > action_server;
+                
+  };//end class
+}//end workspace
+
+#endif SR
+
+/* For the emacs weenies in the crowd.
+Local Variables:
+   c-basic-offset: 2
+End:
+*/
--- shadow_robot/sr_move_arm/src/move_arm_simple_action.cpp
+++ shadow_robot/sr_move_arm/src/move_arm_simple_action.cpp
@@ -1,103 +1,29 @@
-/*
-enum MoveArmState {
-  PLANNING,
-  START_CONTROL,
-  VISUALIZE_PLAN,
-  MONITOR,
-  PAUSE
-};
+/**
+ * @file   move_arm_simple_action.cpp
+ * @author Ugo Cupcic <ugo@shadowrobot.com>, Contact <contact@shadowrobot.com>
+ * @date   Wed Jan 26 10:32:10 2011
+ *
+ * @brief  
+ *
+ */
 
+#include "sr_move_arm/move_arm_simple_action.hpp"
 
-    action_server_.reset(new actionlib::SimpleActionServer<move_arm_msgs::MoveArmAction>(root_handle_, "move_" + group_name, boost::bind(&MoveArm::execute, this, _1)));
-
-
-void execute(const move_arm_msgs::MoveArmGoalConstPtr& goal)
-  {
-    motion_planning_msgs::GetMotionPlan::Request req;	    
-    moveArmGoalToPlannerRequest(goal,req);	    
-    original_request_ = req;
-    ROS_INFO("Received new goal");
-    ros::Rate move_arm_rate(move_arm_frequency_);
-    move_arm_action_result_.contacts.clear();
-    move_arm_action_result_.error_code.val = 0;
-    move_arm_stats_.time_to_execution = ros::Time::now().toSec();
-    move_arm_stats_.time_to_result = ros::Time::now().toSec();
-    while(private_handle_.ok())
-    {	    	    
-      if (action_server_->isPreemptRequested())
-      {
-        move_arm_stats_.preempted = true;
-        if(publish_stats_)
-          publishStats();
-        move_arm_stats_.time_to_execution = ros::Time::now().toSec();
-        move_arm_stats_.time_to_result = ros::Time::now().toSec();
-        if(action_server_->isNewGoalAvailable())
-        {
-          move_arm_action_result_.contacts.clear();
-          move_arm_action_result_.error_code.val = 0;
-          moveArmGoalToPlannerRequest((action_server_->acceptNewGoal()),req);
-          ROS_INFO("Received new goal, will preempt previous goal");
-          original_request_ = req;
-          if (controller_status_ == QUEUED || controller_status_ == ACTIVE)
-            stopTrajectory();
-          state_ = PLANNING;
-        }
-        else               //if we've been preempted explicitly we need to shut things down
-        {
-          ROS_INFO("The move arm action was preempted by the action client. Preempting this goal.");
-          if (controller_status_ == QUEUED || controller_status_ == ACTIVE)
-            stopTrajectory();
-          resetStateMachine();
-          action_server_->setPreempted();
-          return;
-        }
-      }
-
-      //for timing that gives real time even in simulation
-      ros::WallTime start = ros::WallTime::now();
-
-      //the real work on pursuing a goal is done here
-      bool done = executeCycle(req);
-
-
-      if(done)
-      {
-        if(publish_stats_)
-          publishStats();
-        return;
-      }
-
-      ros::WallDuration t_diff = ros::WallTime::now() - start;
-      ROS_DEBUG("Full control cycle time: %.9f\n", t_diff.toSec());
-
-      move_arm_rate.sleep();
-    }	    
-    //if the node is killed then we'll abort and return
-    ROS_INFO("Node was killed, aborting");
-    action_server_->setAborted(move_arm_action_result_);
-  }
-
-
-int main(int argc, char** argv)
+namespace shadowrobot
 {
-  ros::init(argc, argv, "move_arm");
-  
-  ros::AsyncSpinner spinner(1); // Use 1 thread
-  spinner.start();
-  ros::NodeHandle nh("~");
-  std::string group;
-  nh.param<std::string>("group", group, std::string());
-  ROS_INFO("Move arm operating on group %s",group.c_str());    
-  move_arm::MoveArm move_arm(group);
-  if(!move_arm.configure())
+  SrMoveArmSimpleAction::SrMoveArmSimpleAction()
   {
-    ROS_ERROR("Could not configure move arm, exiting");
-    ros::shutdown();
-    return 1;
+    action_server.reset(new actionlib::SimpleActionServer<move_arm_msgs::MoveArmAction>("/arm/move", boost::bind(&SrMoveArmSimpleAction::execute, this, _1)));
   }
-  ROS_INFO("Move arm action started");
-  ros::waitForShutdown();
-    
-  return 0;
+
+  SrMoveArmSimpleAction::execute(const move_arm_msgs::MoveArmGoalConstPtr& Goal)
+  {
+    ROS_ERROR("not implemented yet");
+  }
 }
+
+/* For the emacs weenies in the crowd.
+Local Variables:
+   c-basic-offset: 2
+End:
 */
