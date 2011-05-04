bee519554cf79509da0fa02cfe8f9350964f73bf
Ugo Cupcic <ugo@shadowrobot.com>
2011-05-04 12:26:49 +0100

    headers updated or checked out when running make ; removes writing permission to the headers

--- shadow_robot/sr_hand_palm_edc/sr_edc_ethercat_drivers/update_header.sh
+++ shadow_robot/sr_hand_palm_edc/sr_edc_ethercat_drivers/update_header.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 START_PATH=`pwd`
-cd external
+
+cd "../external"
 
 if [ -d 0220_palm_edc ] ; then
+    chmod -R a+w 0220_palm_edc
     echo "Updating 0220_palm_edc"
     pushd 0220_palm_edc
     svn up
     popd
+
+    #removes write access
+    chmod -R a-w 0220_palm_edc
 else
     echo "Checking out 0220_palm_edc"
     svn co svn://Pericles/Pic32/trunk/nodes/0220_palm_edc
+
+    #removes write access
+    chmod -R a-w 0220_palm_edc
 fi
 
 cd ${START_PATH}
