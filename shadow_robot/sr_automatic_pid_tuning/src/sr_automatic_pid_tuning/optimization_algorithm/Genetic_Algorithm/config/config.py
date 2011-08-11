32ab87affeb0818e37c43f16256a7719023d7f67
Ugo Cupcic <ugo@shadowrobot.com>
2011-08-11 14:09:53 +0100

    lp:824449 implementing a fresh automatic_pid_tuner using the downhill simplex algorithm.

--- shadow_robot/sr_automatic_pid_tuning/src/sr_automatic_pid_tuning/optimization_algorithm/Genetic_Algorithm/config/config.py
+++
@@ -1,3 +0,0 @@
-class FitnessConfig(object):
-    coeff_dic = {'mse':0.1,
-                 'oscillations': 0.1}
--- shadow_robot/sr_automatic_pid_tuning/src/sr_automatic_pid_tuning/optimization_algorithm/Genetic_Algorithm/movement/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ["global_movement", "partial_movement_sinus", "partial_movement_big_steps", "partial_movement_small_steps", "partial_movement_slope"]
