48a5d4bfc8e88ad84932bb584f0c63efa7a2a6d2
Ugo Cupcic <ugo@shadowrobot.com>
2011-08-30 11:09:04 +0100

    Splitting the tree in two different trees: we want one bzr repository per branch.

--- shadow_robot/sr_hand_palm_edc/sr_edc_ethercat_drivers/scripts/flash_all_motors.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {11..19} ; do
-    echo "Flashing motor: $i"
-    rosservice call SimpleMotorFlasher `rospack find sr_external_dependencies`/compiled_firmware/released_firmware/simplemotor.hex $i;
-done
