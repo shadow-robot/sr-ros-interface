b6b1314ef10b9c12607e4fa76d8ae573486e9025
Ugo Cupcic <ugo@shadowrobot.com>
2011-09-16 11:42:35 +0100

    Cleaner SrActuatorState: inherits from pr2_hardware_interface::ActuatorState

--- sr_robot_lib/include/sr_robot_lib/sr_actuator.hpp
+++ sr_robot_lib/include/sr_robot_lib/sr_actuator.hpp
@@ -31,23 +31,11 @@
 
 namespace sr_actuator
 {
-  class SrActuatorState
+  class SrActuatorState : public pr2_hardware_interface::ActuatorState
   {
   public:
     SrActuatorState() :
-      timestamp_(0),
-      device_id_(0),
-      position_(0),
-      velocity_(0),
-      is_enabled_(0),
-      halted_(0),
-      last_commanded_current_(0),
-      last_executed_current_(0),
-      last_measured_current_(0),
-      last_commanded_effort_(0),
-      last_executed_effort_(0),
-      last_measured_effort_(0),
-      motor_voltage_(0),
+      pr2_hardware_interface::ActuatorState(),
       strain_gauge_left_(0),
       strain_gauge_right_(0),
       temperature_(0),
@@ -73,42 +61,6 @@ namespace sr_actuator
       force_control_sign_(0),
       force_control_frequency_(0)
     {}
-
-    /**
-     * The time at which actuator state was measured, relative to the time the ethercat process was started.
-     * Timestamp value is not synchronised with wall time and may be different for different actuators.
-     * For Willow Garage motor controllers, timestamp is made when actuator data is sampled.
-     * sample_timestamp_ will provide better accuracy than ros::Time::now() or robot->getTime()
-     * when using a time difference in calculations based on actuator variables.
-     */
-    ros::Duration sample_timestamp_;
-
-    /** The time at which this actuator state was measured (in seconds).
-     * This value should be same as sample_timestamp_.toSec() for Willow Garage devices.
-     * The timestamp_ variable is being kept around for backwards compatibility, new controllers
-     * should use sample_timestamp_ instead.
-     */
-    double timestamp_;
-
-    int device_id_; //!< Position in EtherCAT chain
-
-    double position_; //!< The position of the motor (in radians)
-    double velocity_; //!< The velocity in radians per second
-
-    bool is_enabled_; //!< Enable status
-    bool halted_; //!< indicates if the motor is halted. A motor can be halted because of voltage or communication problems
-
-    double last_commanded_current_; //!< The current computed based on the effort specified in the ActuatorCommand (in amps)
-    double last_executed_current_; //!< The actual current requested after safety limits were enforced (in amps)
-    double last_measured_current_; //!< The measured current (in amps)
-
-    double last_commanded_effort_; //!< The torque requested in the previous ActuatorCommand (in Nm)
-    double last_executed_effort_; //!< The torque applied after safety limits were enforced (in Nm)
-    double last_measured_effort_; //!< The measured torque (in Nm)
-
-    double max_effort_; //!< Absolute torque limit for actuator (derived from motor current limit). (in Nm)
-
-    double motor_voltage_; //!< Motor voltage (in volts)
 
     unsigned long long int  strain_gauge_left_;
     unsigned long long int strain_gauge_right_;
