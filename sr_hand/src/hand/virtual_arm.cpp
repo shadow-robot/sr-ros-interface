37f75ba15611dd58954697438515c685d8b61b84
Ugo Cupcic <ugo@shadowrobot.com>
2011-10-12 16:38:12 +0100

    Wrong model name for gazebo.

--- sr_hand/src/hand/virtual_arm.cpp
+++ sr_hand/src/hand/virtual_arm.cpp
@@ -125,7 +125,7 @@ void VirtualArm::initializeMap()
     //then we set the ElbowJSwing in the model pose (the model is called arm_and_hand)
     ros::ServiceClient set_pos_client = node.serviceClient<gazebo::SetModelConfiguration>("/gazebo/set_model_configuration");
     gazebo::SetModelConfiguration model_srv;
-    model_srv.request.model_name = "arm_and_hand";
+    model_srv.request.model_name = "shadow_model";
     model_srv.request.test_urdf_param_name = "robot_description";
     model_srv.request.joint_names.push_back("ElbowJSwing");
     model_srv.request.joint_positions.push_back(2.0);
