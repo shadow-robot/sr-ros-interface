1fe6e7db3f39b5a22a5b3a93f58ccc8cea81ea30
Ugo Cupcic <ugo@shadowrobot.com>
2011-10-31 09:31:12 +0000

    Denso arm not Denzo arm.

---
+++ denso_arm/include/denso_arm/denso_arm.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file   denso_arm.hpp
+ * @author Ugo Cupcic <ugo@shadowrobot.com>
+ * @date   Mon Oct 31 09:26:15 2011
+ *
+* Copyright 2011 Shadow Robot Company Ltd.
+*
+* This program is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the Free
+* Software Foundation, either version 2 of the License, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+ * @brief A ROS interface to the DENSO arm.
+ *
+ *
+ */
+
+/* For the emacs weenies in the crowd.
+Local Variables:
+   c-basic-offset: 2
+End:
+*/
