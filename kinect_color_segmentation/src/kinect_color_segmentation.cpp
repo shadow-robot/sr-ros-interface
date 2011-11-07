08651a30839ba957c11021d9f4cf465b56e01aca
Toni Oliver Duran <toni@mallorca>
2011-11-07 20:16:42 +0000

    Some changes in segmentation

--- kinect_color_segmentation/src/kinect_color_segmentation.cpp
+++ kinect_color_segmentation/src/kinect_color_segmentation.cpp
@@ -39,7 +39,7 @@ namespace sr_kinect
     viewer = new pcl_visualization::CloudViewer("Cloud 3D");
     ros::NodeHandle & nh = getNodeHandle();
     sub_ = nh.subscribe<PointCloud >("cloud_in",2, &KinectColorSegmentation::callback, this);
-
+    pub_ = nh.advertise<PointCloud>("seg_output", 1000);
     segmented_pcl = boost::shared_ptr<PointCloud>(new PointCloud() );
   }
 
@@ -51,10 +51,28 @@ namespace sr_kinect
     {
       for(unsigned int j=0; j < cloud->height; ++j)
       {
-        if( i < j)
+//        if((( cloud->at(i,j).rgb & 0xFF0000) > 0x700000)
+//          && (( cloud->at(i,j).rgb & 0xFF0000) < 0xF00000)
+//          &&(( cloud->at(i,j).rgb & 0x00FF00) > 0x007000)
+//          &&(( cloud->at(i,j).rgb & 0x00FF00) < 0x00F000)
+//          &&(( cloud->at(i,j).rgb & 0x0000FF) > 0x000070)
+//          &&(( cloud->at(i,j).rgb & 0x0000FF) < 0x0000F0)        		
+//          )
+		if((( cloud->at(i,j).r) > 0x70)
+				  && (( cloud->at(i,j).r) < 0xC0)
+				  &&(( cloud->at(i,j).g) > 0x70)
+				  &&(( cloud->at(i,j).g) < 0xC0)
+				  &&(( cloud->at(i,j).b) > 0x70)
+				  &&(( cloud->at(i,j).b) < 0xC0)        		
+				  )
           segmented_pcl->push_back( cloud->at(i,j) );
       }
     }
+    segmented_pcl->header.frame_id = cloud->header.frame_id;
+    
+    pub_.publish(segmented_pcl);
+    
+    ros::spinOnce();
 
     if(!viewer->wasStopped())
     {
