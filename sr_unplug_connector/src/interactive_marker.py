2edde99d3f9e890a987ad35010155850fc048d51
Ugo Cupcic <ugo@shadowrobot.com>
2011-11-14 14:19:25 +0000

    Takes a list of names as input and displays the corresponding markers.

--- sr_unplug_connector/src/interactive_marker.py
+++ sr_unplug_connector/src/interactive_marker.py
@@ -22,80 +22,103 @@ import rospy
 from interactive_markers.interactive_marker_server import *
 
 class InteractiveConnectorSelector(object):
-    def __init__(self, object_name, index_object):
+    def __init__(self, object_names):
         # create an interactive marker server on the topic namespace simple_marker
         self.server = InteractiveMarkerServer("select_connector")
 
-        self.object_name = object_name
+        self.object_names = object_names
 
+        self.int_markers = {}
+        self.object_markers = {}
+        self.object_controls = {}
+        self.select_controls = {}
+
+        for object_name in self.object_names:
+            self.create_marker( object_name )
+
+        rospy.spin()
+
+    def create_marker(self, object_name):
         # create an interactive marker for our server
-        self.int_marker = InteractiveMarker()
-        self.int_marker.header.frame_id = "/"+object_name
-        self.int_marker.name = object_name
-        self.int_marker.description = "Select this object"
-        self.int_marker.pose.position.z = 0.05
+        int_marker = InteractiveMarker()
+        int_marker.header.frame_id = "/"+object_name
+        int_marker.name = object_name
+        int_marker.description = "Select this object"
+        int_marker.pose.position.z = -0.3
 
+        self.int_markers[ object_name ] = int_marker
 
-        # create a grey box marker
-        self.object_marker = Marker()
-        self.object_marker.type = Marker.SPHERE
-        self.object_marker.pose.position.z = 0.25
+        # create a marker over the object
+        object_marker = Marker()
+        object_marker.type = Marker.SPHERE
+        object_marker.pose.position.z = 0.6
 
-        self.object_marker.scale.x = 0.1
-        self.object_marker.scale.y = 0.1
-        self.object_marker.scale.z = 0.1
-        self.object_marker.color.r = 0.6
-        self.object_marker.color.g = 0.02
-        self.object_marker.color.b = 1.0
-        self.object_marker.color.a = 1.0
+        object_marker.scale.x = 0.1
+        object_marker.scale.y = 0.1
+        object_marker.scale.z = 0.1
+        object_marker.color.r = 0.6
+        object_marker.color.g = 0.02
+        object_marker.color.b = 1.0
+        object_marker.color.a = 1.0
+        self.object_markers[object_name] = object_marker
 
         # create a non-interactive control which contains the box
-        self.object_control = InteractiveMarkerControl()
-        self.object_control.always_visible = True
-        self.object_control.markers.append( self.object_marker )
+        object_control = InteractiveMarkerControl()
+        object_control.always_visible = True
+        object_control.markers.append( object_marker )
+
+        self.object_controls[object_name] = object_control
 
         # add the control to the interactive marker
-        self.int_marker.controls.append( self.object_control )
+        self.int_markers[object_name].controls.append( object_control )
 
         # create a control which will move the box
         # this control does not contain any markers,
         # which will cause RViz to insert two arrows
-        self.select_control = InteractiveMarkerControl()
-        self.select_control.name = "select_"+object_name
-        self.select_control.description = "Select this object: CLICK ME"
-        self.select_control.interaction_mode = InteractiveMarkerControl.MENU
+        select_control = InteractiveMarkerControl()
+        select_control.name = "select_"+object_name
+        select_control.description = "______\n|CLICK|"
+        select_control.interaction_mode = InteractiveMarkerControl.MENU
         #select_control.menu_entries.push_back()
+        self.select_controls[object_name] = select_control
 
         # add the control to the interactive marker
-        self.int_marker.controls.append(self.select_control);
+        self.int_markers[object_name].controls.append(select_control);
 
         # add the interactive marker to our collection &
         # tell the server to call processFeedback() when feedback arrives for it
-        self.server.insert(self.int_marker, self.processFeedback)
+        self.server.insert(self.int_markers[object_name], self.processFeedback)
 
         # 'commit' changes and send to all clients
         self.server.applyChanges()
 
-        rospy.spin()
-
     def processFeedback(self, feedback):
-        #p = feedback.menu_entry_id
-        self.object_control.markers.remove( self.object_marker )
+        selected_name = feedback.marker_name
+        for index, name in enumerate(self.object_controls):
+            self.object_controls[name].markers.remove( self.object_markers[name] )
 
-        self.object_marker.pose.position.z = 0.25
+            if name == selected_name:
+                self.object_markers[name].pose.position.z = 0.65
 
-        self.object_marker.scale.x = 0.15
-        self.object_marker.scale.y = 0.15
-        self.object_marker.scale.z = 0.15
-        self.object_marker.color.r = 0.32
-        self.object_marker.color.g = 0.83
-        self.object_marker.color.b = 0.15
-        self.object_marker.color.a = 1.0
+                self.object_markers[name].scale.x = 0.15
+                self.object_markers[name].scale.y = 0.15
+                self.object_markers[name].scale.z = 0.15
+                self.object_markers[name].color.r = 0.32
+                self.object_markers[name].color.g = 0.83
+                self.object_markers[name].color.b = 0.15
+            else:
+                self.object_markers[name].pose.position.z = 0.6
 
-        self.object_control.markers.append( self.object_marker )
-        print "clicked"
+                self.object_markers[name].scale.x = 0.1
+                self.object_markers[name].scale.y = 0.1
+                self.object_markers[name].scale.z = 0.1
+                self.object_markers[name].color.r = 0.6
+                self.object_markers[name].color.g = 0.02
+                self.object_markers[name].color.b = 1.0
 
-        self.server.insert(self.int_marker)
+            self.object_controls[name].markers.append( self.object_markers[name] )
+
+            self.server.insert(self.int_markers[name])
 
         self.server.applyChanges()
 
@@ -103,5 +126,5 @@ class InteractiveConnectorSelector(object):
 if __name__=="__main__":
     rospy.init_node("simple_marker")
 
-    int_mark = InteractiveConnectorSelector("Cup.RobotAreCoolCup", 0)
+    int_mark = InteractiveConnectorSelector(["Cup.RobotAreCoolCup", "srh/position/forearm_motor"])
 
