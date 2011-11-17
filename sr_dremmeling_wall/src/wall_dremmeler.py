c4adec1bbfed205e052ecdbbf04464b015b8cc38
Ugo Cupcic <ugo@shadowrobot.com>
2011-11-17 16:47:53 +0000

    Added the calls to the services in the sr_dremmeling_wall

--- sr_dremmeling_wall/src/wall_dremmeler.py
+++ sr_dremmeling_wall/src/wall_dremmeler.py
@@ -22,6 +22,7 @@ import actionlib
 
 import denso_msgs.msg
 from geometry_msgs.msg import Pose, Quaternion
+from kinect_color_segmentation.srv import WallNormale, SurfaceToDremmel
 
 class WallDremmeler(object):
     """
@@ -33,7 +34,10 @@ class WallDremmeler(object):
         rospy.init_node('sr_dremmeling_wall')
 
         self.trajectory_client = actionlib.SimpleActionClient('/denso_arm/trajectory', denso_msgs.msg.TrajectoryAction)
-        self.trajectory_client.wait_for_server()
+        #self.trajectory_client.wait_for_server()
+
+        self.surface_to_dremmel_server = rospy.ServiceProxy( '/kinect_color_segmentation/segment', SurfaceToDremmel)
+        self.wall_orientation_server = rospy.ServiceProxy( '/kinect_color_segmentation/get_wall_normale', WallNormale)
 
         rospy.spin()
 
@@ -41,18 +45,31 @@ class WallDremmeler(object):
         """
         """
         #First we get the segmented points.
+        rospy.loginfo("segmenting the point cloud")
         segmented_points = []
-        print "TODO: get a list of segmented points - call your service of the type kinect_color_segmentation.srv.SurfaceToDremmel"
+        try:
+            res = self.surface_to_dremmel_server()
+            segmented_points = res.points
+        except:
+            rospy.logerr("Couldn't segment the point cloud")
+            return
 
         #Then we get the normal for the wall
-        wall_normal = Quaternion()
+        rospy.loginfo("Getting the wall normale")
+        wall_normale = Quaternion()
         wall_link = ""
-        print "TODO: retrieve the Pose for the wall normal - call your service of the type kinect_color_segmentation.srv.WallOrientation.srv"
+        try:
+            res = self.wall_orientation_server()
+            wall_normale = res.normale
+            wall_link = res.frame_name
+        except:
+            rospy.logerr("Couldn't get the wall normale")
 
         #We build a list of poses to send to the hand.
-        list_of_poses = self.build_poses( segmented_points, wall_normal, wall_link )
+        list_of_poses = self.build_poses( segmented_points, wall_normale, wall_link )
 
         #now we send this to the arm
+        rospy.loginfo("Sending the list of poses to the arm")
         goal = denso_msgs.msg.TrajectoryGoal
         goal.trajectory = list_of_poses
 
