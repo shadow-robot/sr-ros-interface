bad19d733d7e4168e7ae294de12622393c1bbb58
Ugo Cupcic <ugo@shadowrobot.com>
2011-12-08 11:13:04 +0000

    Fixed merge mistake.

--- sr_hardware_interface/include/sr_hardware_interface/tactile_sensors.hpp
+++ sr_hardware_interface/include/sr_hardware_interface/tactile_sensors.hpp
@@ -31,6 +31,11 @@
 #include <vector>
 
 #include <boost/array.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/find_iterator.hpp>
+#include <sstream>
+
+#include <ros/ros.h>
 
 namespace tactiles
 {
@@ -41,10 +46,14 @@ namespace tactiles
 
     GenericTactileData(bool tactile_data_valid, int sample_frequency,
                        std::string manufacturer, std::string serial_number,
-                       int software_version, int pcb_version)
+                       int software_version_current, int software_version_server,
+                       bool software_version_modified, int pcb_version)
       : tactile_data_valid(tactile_data_valid), sample_frequency(sample_frequency),
         manufacturer(manufacturer), serial_number(serial_number),
-        software_version(software_version), pcb_version(pcb_version)
+        software_version_current(software_version_current),
+        software_version_server(software_version_server),
+        software_version_modified(software_version_modified),
+        pcb_version(pcb_version)
     {};
 
     ~GenericTactileData() {};
@@ -56,8 +65,65 @@ namespace tactiles
     std::string manufacturer;
     std::string serial_number;
 
-    int software_version;
+    int software_version_current;
+    int software_version_server;
+    bool software_version_modified;
+
+    /**
+     * Parses the version string received
+     *  from the tactiles and fill in the
+     *  variables.
+     *
+     * @version The raw version string.
+     */
+    void set_software_version( std::string version )
+    {
+      //split the string to fill the different versions
+      std::vector<std::string> splitted_string;
+      boost::split(splitted_string, version, boost::is_any_of("\n"));
+
+      BOOST_ASSERT(splitted_string.size() >= 3);
+      software_version_current = convertToInt(splitted_string[0]);
+      software_version_server = convertToInt(splitted_string[1]);
+
+      if( splitted_string[2] == "No")
+        software_version_modified = false;
+      else
+        software_version_modified = true;
+    };
+
+    /**
+     * Formats the software version for the
+     *  diagnostics.
+     *
+     * @return the formatted string
+     */
+    virtual std::string get_software_version()
+    {
+      //concatenate versions in a string.
+      std::string full_version;
+
+      std::stringstream ss;
+      if( software_version_modified )
+        ss << "current: " << software_version_current << " / server: " << software_version_server << " / MODIFIED";
+      else
+        ss << "current: " << software_version_current << " / server: " << software_version_server << " / not modified";
+
+      full_version = ss.str();
+
+      return full_version;
+    };
+
     int pcb_version;
+
+    inline double convertToInt(std::string const& s)
+    {
+      std::istringstream i(s);
+      int x;
+      if (!(i >> x))
+        x = -1;
+      return x;
+    }
   };
 
   class PST3Data
@@ -71,7 +137,10 @@ namespace tactiles
     PST3Data(const PST3Data& pst3)
       : GenericTactileData(pst3.tactile_data_valid, pst3.sample_frequency,
                            pst3.manufacturer, pst3.serial_number,
-                           pst3.software_version, pst3.pcb_version),
+                           pst3.software_version_current,
+                           pst3.software_version_server,
+                           pst3.software_version_modified,
+                           pst3.pcb_version),
         pressure(pst3.pressure), temperature(pst3.temperature),
         debug_1(pst3.debug_1), debug_2(pst3.debug_2),
         pressure_raw(pst3.pressure_raw), zero_tracking(pst3.zero_tracking), dac_value(pst3.dac_value)
@@ -81,7 +150,10 @@ namespace tactiles
     PST3Data(const GenericTactileData& gtd)
       : GenericTactileData(gtd.tactile_data_valid, gtd.sample_frequency,
                            gtd.manufacturer, gtd.serial_number,
-                           gtd.software_version, gtd.pcb_version)
+                           gtd.software_version_current,
+                           gtd.software_version_server,
+                           gtd.software_version_modified,
+                           gtd.pcb_version)
     {};
 
     ~PST3Data() {};
@@ -95,6 +167,25 @@ namespace tactiles
     int zero_tracking;
 
     int dac_value;
+
+    /**
+     * Formats the software version for the
+     *  diagnostics.
+     *
+     * @return the formatted string
+     */
+    virtual std::string get_software_version()
+    {
+      //concatenate versions in a string.
+      std::string full_version;
+
+      std::stringstream ss;
+      ss << "current: " << software_version_current;
+
+      full_version = ss.str();
+
+      return full_version;
+    };
   };
 
   class BiotacData
@@ -108,7 +199,10 @@ namespace tactiles
     BiotacData(const BiotacData& btac)
       : GenericTactileData(btac.tactile_data_valid, btac.sample_frequency,
                            btac.manufacturer, btac.serial_number,
-                           btac.software_version, btac.pcb_version),
+                           btac.software_version_current,
+                           btac.software_version_server,
+                           btac.software_version_modified,
+                           btac.pcb_version),
         pac0(btac.pac0), pac1(btac.pac1),
         pdc(btac.pdc), tac(btac.tac),
         tdc(btac.tdc)
@@ -122,7 +216,10 @@ namespace tactiles
     BiotacData(const GenericTactileData& gtd)
       : GenericTactileData(gtd.tactile_data_valid, gtd.sample_frequency,
                            gtd.manufacturer, gtd.serial_number,
-                           gtd.software_version, gtd.pcb_version)
+                           gtd.software_version_current,
+                           gtd.software_version_server,
+                           gtd.software_version_modified,
+                           gtd.pcb_version)
     {};
 
     ~BiotacData() {};
