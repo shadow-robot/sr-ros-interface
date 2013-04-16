aebd306661553c1dd6c069aefa644bbffe958dcf
Ugo Cupcic <ugo@shadowrobot.com>
2013-04-16 17:07:02 +0200

    clarified message

--- sr_self_test/src/manual_test.cpp
+++ sr_self_test/src/manual_test.cpp
@@ -44,14 +44,14 @@ namespace shadow_robot
     //TODO: start rxplot for the tactiles
     // ask the user to press the tactiles
     sr_robot_msgs::ManualSelfTest tactile_srv;
-    tactile_srv.request.message = "Please press on the tactile sensors one after the other. Check that they react on the displayed plot.";
+    tactile_srv.request.message = "Please press on the tactile sensors one after the other. Check that they react using rxplot.";
     user_input_client_.call(tactile_srv);
 
     //Run Calibration test
     //TODO: start rviz
     // ask the user to check the calibration visually
     sr_robot_msgs::ManualSelfTest calibration_srv;
-    calibration_srv.request.message = "Please check that the positions of the joints in the 3d model of the hand match those in the real hand.";
+    calibration_srv.request.message = "Please check that the positions of the joints in the 3d model of the hand (using rviz) match those in the real hand.";
     user_input_client_.call(calibration_srv);
 
     if( tactile_srv.response.ok && calibration_srv.response.ok )
