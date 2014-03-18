16a88a438fbd5e7e896e1e7987f9d51ab2357d7a
Toni Oliver <toni.oliver.d@gmail.com>
2014-03-18 14:46:44 +0000

    Allow several concurrent interpolators. Coinciding joints in existing interpolators are overriden by new calls to move_hand.

--- sr_hand/scripts/sr_hand/shadowhand_commander.py
+++ sr_hand/scripts/sr_hand/shadowhand_commander.py
@@ -21,7 +21,8 @@ import threading
 
 from sr_robot_msgs.msg import sendupdate, joint
 from sr_hand.shadowhand_ros import ShadowHand_ROS
-from Grasp import Grasp
+from sr_hand.grasps_interpoler import GraspInterpoler
+from sr_hand.Grasp import Grasp
 
 """
 Module to provide a quick and easy way to script the Shadow Hand.
@@ -60,10 +61,9 @@ class Commander(object):
     def __init__(self):
         # Mutex to avoid two calls of self.move_hand to be active at the same time
         self.mutex = threading.Lock()
-        # Mutex to control access to the variable self.hand_mvt_stopped
-        self.mutex_stop_mov = threading.Lock()
-        # This is used to stop the currently running (in case there is one) hand interpolation thread. We override the still running command with the new one.
-        self.hand_mvt_stopped = True
+
+        # This is used to store the grasp interpolators for the different threads.
+        self.grasp_interpolators = {}
 
         # Shadow hand setup
         self.hand = ShadowHand_ROS()
@@ -72,7 +72,7 @@ class Commander(object):
         # the interpoler will use 10 steps
         self.hand_interpolation_period = 0.01
         if self.hand.check_hand_type() == "gazebo":
-            # Interpolation is far too slow under simulation compared to real hand
+            # Interpolation is slower under simulation compared to real hand
             self.hand_interpolation_period = 0.1
 
 
@@ -80,28 +80,23 @@ class Commander(object):
         """
         Move the Shadow Hand.
 
-        This is call is non blocking. It will return before the motion has finished.
+        This call is non blocking. It will return before the motion has finished.
 
-        If called with interpolation time of 0.0 or smaller than
-        self.hand_interpolation_period, sending another target while the hand
-        is still moving will cause the new move to take over). So you will need
-        to add sleeps to control the motion.
-
+        If called with interpolation time of 0.0 the joints will move to the target position
+        at maximum velocity (i.e. the max. vel. allowed by the position controller for that joint)
+        
         If called with interpolation time greater than self.hand_interpolation_period,
         a separate thread will deal with the interpolation time and will keep 
         sending targets to the hand until the last target is sent.
-        Sending another command while the previous one is still going will cause the new
-        move_hand call to block until the previous 
-        move to take over). So you will need to add sleeps to control the
-        motion.
+        
+        If move_hand() is called again before the previous movements have finished,
+        the new call will override only the conciding joints. The others will keep moving
+        to their previous targets at their previous velocity.
 
         @param joints - Dictionary of joint names in the keys and angles in
-        degrees in the values.
+        degrees in the values. The key interpolation_time gives the time in seconds that 
+        the movement will last.
         """
-        #This allows to end the self.hand_interpolation_thread in case it is running
-        self.mutex_stop_mov.acquire()
-        self.hand_mvt_stopped = True
-        self.mutex_stop_mov.release()
         
         interpolation_time = 0.0
         if 'interpolation_time' in joints:
@@ -109,42 +104,68 @@ class Commander(object):
 
         if interpolation_time == 0.0:
             self.mutex.acquire()
-            self.hand.sendupdate_from_dict(joints)
+            self._prune_interpolators(joints)            
             self.mutex.release()
+            self.hand.sendupdate_from_dict(joints)
         else:
-            self.hand_interpolation_thread = threading.Thread(
-                    target=self._move_hand_interpolation,
-                    args=(joints, interpolation_time)).start()
+            threading.Thread(target=self._move_hand_interpolation,
+                             args=(joints, interpolation_time)).start()
+
 
     def _move_hand_interpolation(self, joints, interpolation_time=1.0):
-        rospy.logdebug("call interpol")
-        self.mutex.acquire()
-        self.hand_mvt_stopped = False
+        """
+        Remove the coinciding joints from the currently running interpolators.
+        It actually removes the joints from the 'grasp_to' list of the interpolator.
+
+        @param joints - Dictionary of joint names in the keys and angles in
+        degrees in the values.
+        @param interpolation_time - A float. Interpolation time in seconds.
+        """
+        
+        rospy.logdebug("call interpolation")
+        thread_name = threading.current_thread().name
         try:
-            rospy.logdebug("start interpol")
+            self.mutex.acquire()
+            self._prune_interpolators(joints)
+            self.mutex.release()
+          
+            rospy.logdebug("start interpolation")
             current_grasp = Grasp()
             current_grasp.joints_and_positions = self.hand.read_all_current_positions()
             target_grasp = Grasp()
             target_grasp.joints_and_positions = joints
 
-            interpoler = self.hand.create_grasp_interpoler(current_grasp, target_grasp)
+            interpolator = GraspInterpoler(current_grasp, target_grasp)
+            self.grasp_interpolators[thread_name] = interpolator
+            
             r = rospy.Rate(1.0 / self.hand_interpolation_period)
+            
             for interpolation in range (1, int(interpolation_time / self.hand_interpolation_period) + 1):
-                #This allows to end this thread in case a new move_hand command is called
-                self.mutex_stop_mov.acquire()
-                if self.hand_mvt_stopped:
-                    self.mutex_stop_mov.release()
-                    rospy.loginfo("Interpolation overriden. A new hand command has overriden the ongoing movement")
-                    return
-                self.mutex_stop_mov.release()
-
-                targets_to_send = self.hand.grasp_interpoler.interpolate(100.0 * interpolation * self.hand_interpolation_period / interpolation_time)
+                self.mutex.acquire()
+                targets_to_send = interpolator.interpolate(100.0 * interpolation * self.hand_interpolation_period / interpolation_time)
+                self.mutex.release()
                 self.hand.sendupdate_from_dict(targets_to_send)
                 #rospy.loginfo("sent cmd n: %s" %(str(interpolation), ))
                 r.sleep()
         finally:
+            self.mutex.acquire()
+            self.grasp_interpolators.pop(thread_name, None)
             self.mutex.release()
-            rospy.logdebug("end interpol")
+            rospy.logdebug("end interpolation")
+
+
+    def _prune_interpolators(self, joints):
+        """
+        Remove the coinciding joints from the currently running interpolators.
+        It actually removes the joints from the 'grasp_to' list of the interpolator.
+
+        @param joints - Dictionary of joint names in the keys and angles in
+        degrees in the values.
+        """
+        for thread_id in self.grasp_interpolators.keys():
+            for joint_name in joints.keys():
+                self.grasp_interpolators[thread_id].grasp_to.joints_and_positions.pop(joint_name, None)
+
 
     def get_hand_position(self):
         return self.hand.read_all_current_positions()
