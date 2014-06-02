f75e7cd6f4846e402909e50c1b77b1af25804f2f
Yi Li <yi@shadowrobot.com>
2014-06-02 15:34:28 +0100

    Loop in standalone_example.cpp.

--- sr_standalone_example/standalone_example.cpp
+++ sr_standalone_example/standalone_example.cpp
@@ -1,14 +1,27 @@
 #include <sr_standalone/shadow_hand.hpp>
+#include <iostream>
+#include <boost/date_time.hpp>
+#include <boost/thread.hpp>
 
 using namespace shadow_robot_standalone;
 
 int main(int argc, char** argv)
 {
   ShadowHand hand(argc, argv);
-  JointStates jss = hand.get_joint_states();
+  const JointStates & jss = hand.get_joint_states();
 
-  std::vector<std::string> js_names = jss.names;
-  std::vector<double> positions;
+  for (std::size_t counter = 0; counter < 100; ++counter)
+  {
+    for (std::size_t i = 0; i < jss.names.size(); ++i)
+      std::cout << "Joint State Name = " << jss.names[i] << std::endl;
+
+    for (std::size_t i = 0; i < jss.positions.size(); ++i)
+      std::cout << "Joint State Positions = " << jss.positions[i] << std::endl;
+
+    std::cout << "\nSleeping...\n";
+    boost::posix_time::seconds dura(1);
+    boost::this_thread::sleep(dura);
+  }
 
   return 0;
 }
