5b568dc2a4676f3328c48878c9622b736a6b5edb
Ugo Cupcic <ugo@shadowrobot.com>
2014-09-04 16:54:06 +0200

    adding todos for wait for calibrated

--- sr_gazebo_plugins/src/gazebo_ros_controller_manager.cpp
+++ sr_gazebo_plugins/src/gazebo_ros_controller_manager.cpp
@@ -196,6 +196,8 @@ void GazeboRosControllerManager::ResetChild()
 
 void GazeboRosControllerManager::UpdateChild()
 {
+  //TODO publish calibrated = true on first call
+
   if (world->IsPaused() || !fake_state_ || !cm_)
     return;
 
