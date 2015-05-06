d82ecb1da3afb3ba7d04ab41e9096a44c48dfaf9
Ugo Cupcic <ugo@shadowrobot.com>
2015-05-06 08:10:52 +0200

    fixing sr_utilities

--- sr_utilities/scripts/get_joint_states.py
+++ sr_utilities/scripts/get_joint_states.py
@@ -6,30 +6,27 @@ import thread
 
 RATE = 100
 
-class GetJointState:
+class GetJointState(object):
     def __init__(self):
         rospy.init_node('get_joint_state_service', anonymous=True)
 
         self.subs_1 = rospy.Subscriber("/joint_states", JointState, self.callback1)
-	self.serv = rospy.Service('/getJointState', getJointState, self.getJointStateCB)
-
-#        self.pub = rospy.Publisher("/joint_states", JointState)
+        self.serv = rospy.Service('/getJointState', getJointState, self.getJointStateCB)
 
         self.joint_state_msg = JointState()
 
         self.mutex = thread.allocate_lock()
 
-        r = rospy.Rate( RATE )
+        r = rospy.Rate(RATE)
         while not rospy.is_shutdown():
-            #self.publish()
             r.sleep()
 
     def callback1(self, data):
         self.joint_state_msg = data
 
     def getJointStateCB(self,req):
-        res=self.joint_state_msg
-	return res
+        res = self.joint_state_msg
+        return res
 
 if __name__ == '__main__':
     service = GetJointState()
