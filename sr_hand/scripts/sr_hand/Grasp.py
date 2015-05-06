a662c5fc5279f45a2af792468ce6fd4ca281df3f
Ugo Cupcic <ugo@shadowrobot.com>
2015-05-06 08:07:50 +0200

    fixing sr_hand Grasp

--- sr_hand/scripts/sr_hand/Grasp.py
+++ sr_hand/scripts/sr_hand/Grasp.py
@@ -18,18 +18,19 @@
 
 VERBOSE = 1
 
-class Grasp:
-	def __init__(self):
-		self.grasp_name = ""
-		self.joints_and_positions = {}
+class Grasp(object):
 
-	def display_grasp(self):
-		print self.grasp_name
-		print self.joints_and_positions
+    def __init__(self):
+        self.grasp_name = ""
+        self.joints_and_positions = {}
 
-	def convert_to_xml(self):
-		grasp = '	<grasp name="'+self.grasp_name+'">'+'\n'
-		for key, value in self.joints_and_positions.items():
-			grasp = grasp + '		<joint name="'+str(key)+'">'+str(value)+'</joint>\n'
-		grasp = grasp + '	</grasp>'+'\n'
-		return grasp
+    def display_grasp(self):
+        print self.grasp_name
+        print self.joints_and_positions
+
+    def convert_to_xml(self):
+        grasp = '	<grasp name="'+self.grasp_name+'">'+'\n'
+        for key, value in self.joints_and_positions.items():
+            grasp = grasp + '		<joint name="'+str(key)+'">'+str(value)+'</joint>\n'
+        grasp = grasp + '	</grasp>'+'\n'
+        return grasp
