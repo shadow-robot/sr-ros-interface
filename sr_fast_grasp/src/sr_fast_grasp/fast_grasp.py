e9044961f578ad2e5a0474944f1785edeedbe16f
dg <dg@shadowrobot.com>
2015-07-22 11:40:38 +0100

    added start of package

---
+++ sr_fast_grasp/src/sr_fast_grasp/fast_grasp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import rospy
+from shape_msgs.msg import SolidPrimative
+class SrFastGrasp:
+    def __init__(self):
+        pass
+    def start(self):
+        pass
+    def __bounding_box_cb(self, request):
+        rospy.loginfo("got box")
+
+if "__main__" == __name__ :
+    rospy.init_node('sr_fast_grasp')
+    grasp = SrFastGrasp()
+    grasp.start()
+    rospy.spin()
