9e99416aa7fee13ba17d96daf54de644452a9dad
dg <dg@shadowrobot.com>
2015-07-22 15:30:18 +0100

    chooses major axis

--- sr_fast_grasp/src/sr_fast_grasp/fast_grasp.py
+++ sr_fast_grasp/src/sr_fast_grasp/fast_grasp.py
@@ -12,18 +12,36 @@ class SrFastGrasp:
         self.__grasp_server = rospy.Service("grasp_from_bounding_box",
                                             GetFastGraspFromBoundingBox,
                                             self.__bounding_box_cb)
+        self.__default_grasp = 'super_amazing_grasp'
 
     def __bounding_box_cb(self, request):
-        rospy.loginfo("got box")
-        marker = self.__get_marker_from_box(request.bounding_box,
-                                            request.pose)
-        self.__marker_pub.publish(marker)
+        box = request.bounding_box
+        pose = request.pose
+
+        if SolidPrimitive.BOX != box.type:
+            rospy.logerr("Bounding volume must be a BOX.")
+            return None
+
+        return self.__select_grasp(box,pose)
+
+    def __select_grasp(self, box, pose):
+        major_axis = self.__get_major_axis(box)
+        rospy.loginfo(major_axis)
         return Grasp()
+
+    def __get_major_axis(self, box):
+        m = max(box.dimensions)
+        max_index = [i for i,j in enumerate(box.dimensions) if j == m]
+        return max_index[-1]  # Get the LAST axis with max val.
+
+    def __send_marker_to_rvix(self, box, pose):
+        marker = self.__get_marker_from_box(box,pose)
+        self.__marker_pub.publish(marker)
 
     def __get_marker_from_box(self, box, pose):
         marker = Marker()
         marker.pose = pose.pose
-        marker.header.frame_id = '/world' #pose.header.frame_id
+        marker.header.frame_id = pose.header.frame_id
 
         marker.scale.x = box.dimensions[SolidPrimitive.BOX_X]
         marker.scale.y = box.dimensions[SolidPrimitive.BOX_Y]
