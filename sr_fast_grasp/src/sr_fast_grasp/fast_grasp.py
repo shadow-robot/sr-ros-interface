9dcaa34a7edb61c91dfaa60bbbe3d2f9abdf740f
dg <dg@shadowrobot.com>
2015-07-23 10:52:14 +0100

    progress

--- sr_fast_grasp/src/sr_fast_grasp/fast_grasp.py
+++ sr_fast_grasp/src/sr_fast_grasp/fast_grasp.py
@@ -5,6 +5,10 @@ from shape_msgs.msg         import SolidPrimitive
 from visualization_msgs.msg import Marker
 from sr_robot_msgs.srv      import GetFastGraspFromBoundingBox
 from moveit_msgs.msg        import Grasp
+from moveit_commander       import MoveGroupCommander
+from geometry_msgs.msg      import Pose
+from moveit_msgs.srv import GetRobotStateFromWarehouse as GetState
+
 class SrFastGrasp:
     def __init__(self):
         self.__marker_pub   = rospy.Publisher("visualization_marker",
@@ -13,28 +17,51 @@ class SrFastGrasp:
                                             GetFastGraspFromBoundingBox,
                                             self.__bounding_box_cb)
         self.__default_grasp = 'super_amazing_grasp'
+        self.__grasp_db_cnxn = rospy.ServiceProxy(
+            '/moveit_warehouse_services/get_robot_state', GetState)
+        ##self.__group = MoveGroupCommander("hand")
 
     def __bounding_box_cb(self, request):
-        box = request.bounding_box
+        box  = request.bounding_box
         pose = request.pose
-
         if SolidPrimitive.BOX != box.type:
             rospy.logerr("Bounding volume must be a BOX.")
             return None
+        self.__send_marker_to_rviz(box, pose)
+        grasp_name = self.__select_grasp()
+        return self.__get_grasp(grasp_name)
 
-        return self.__select_grasp(box,pose)
+    def __select_grasp(self):
+        return self.__default_grasp
 
-    def __select_grasp(self, box, pose):
+    def __get_grasp(self, name):
+        open_state   = self.__get_state(name + "_open").joint_state
+        closed_state  = self.__get_state(name + "_closed").joint_state
+        current_state = self.__group.get_current_joint_values()
+
+        (pre_pose, fraction) = self.__group.plan([current_state, open_state])
+        (pose,     fraction) = self.__group.plan([open_state, close_state])
+        #  TODO(dg-shadow) check trajs worked
+
+        grasp = Grasp()
+        grasp.id = name
+        grasp.pre_grasp_posture = pre_pose
+        grasp.grasp_posture     = pose
+        #fill grasp
+
+        return Grasp()
+
+    def __orient_grasp(self, box, pose):
         major_axis = self.__get_major_axis(box)
         rospy.loginfo(major_axis)
-        return Grasp()
+        return Pose
 
     def __get_major_axis(self, box):
         m = max(box.dimensions)
         max_index = [i for i,j in enumerate(box.dimensions) if j == m]
         return max_index[-1]  # Get the LAST axis with max val.
 
-    def __send_marker_to_rvix(self, box, pose):
+    def __send_marker_to_rviz(self, box, pose):
         marker = self.__get_marker_from_box(box,pose)
         self.__marker_pub.publish(marker)
 
@@ -50,7 +77,7 @@ class SrFastGrasp:
         marker.color.r = 1.0
         marker.color.g = 0.0
         marker.color.b = 0.0
-        marker.color.a = 1.0
+        marker.color.a = 0.5
 
         marker.lifetime = rospy.rostime.Duration()
         marker.type = Marker.CUBE
