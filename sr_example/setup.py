a93c446092b482885b7b11f213d8b7c8e51d1548
Ugo Cupcic <ugo@shadowrobot.com>
2015-08-28 15:36:41 +0200

    removing all packages that are now in separate repositories

--- sr_example/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['sr_example'],
-    package_dir={'': 'scripts'})
-
-setup(**setup_args)
--- sr_hand/examples/README.txt
+++
@@ -1,1 +0,0 @@
-The examples in this directory have been moved to the package sr_example
