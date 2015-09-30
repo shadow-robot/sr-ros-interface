e8218ca09aeb5ffd5917a0de0ffec9c85c50477e
Ugo Cupcic <ugo@shadowrobot.com>
2015-09-30 14:13:13 +0200

    using pandoc for on the fly markdown to rst conversion for now

--- docs/generate_doc.sh
+++ docs/generate_doc.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-for file in `find ../../ -name "*.md"`; do mkdir -p shadow_robot/docs/`dirname ${file}` ; cp ${file} shadow_robot/docs/`dirname ${file}`/ ; done
+PATH_TO_GENERATED_DOCS=source/generated
 
+rm -rf ${PATH_TO_GENERATED_DOCS}
+mkdir -p ${PATH_TO_GENERATED_DOCS}
+
+for FILE in `find ../../ -name "*.md"`; do
+    echo "--------_"
+    FILE_BASENAME=`basename -s .md ${FILE}`
+    PATH_TO_FILE=`dirname ${FILE}`
+    echo ${PATH_TO_FILE} "  " ${FILE_BASENAME}
+    STRIPPED_PATH_TO_FILE=`echo ${PATH_TO_FILE} | sed 's;../../;;g'`
+
+    mkdir -p ${PATH_TO_GENERATED_DOCS}/${STRIPPED_PATH_TO_FILE};
+
+    PATH_TO_RST=${PATH_TO_GENERATED_DOCS}/${STRIPPED_PATH_TO_FILE}/${FILE_BASENAME}.rst
+
+    echo "Copying ${FILE} to ${PATH_TO_RST}"
+    pandoc --from=markdown --to=rst --output=${PATH_TO_RST} ${FILE}
+ done
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -11,7 +11,8 @@ Contents:
 .. toctree::
    :maxdepth: 2
 
-
+   Getting Started <generated/shadow_robot/README.rst>
+   Install Instructions <generated/shadow_robot/INSTALL.rst>
 
 Indices and tables
 ------------------
